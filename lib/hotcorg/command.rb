9f08d334d4bcbd10f19a66a1929e19a9d8190551
= <>
2018-12-02 17:09:24 +0900

    changed definition of trial class to get cpu temp with iStats module

--- lib/hotcorg/command.rb
+++ lib/hotcorg/command.rb
@@ -1,5 +1,10 @@
-module Hotcorg
-    class Command
+require 'iStats'
 
+
+module Hotcorg
+    class Command extend CPU_STATS
+        def self.cpu_temp
+            get_cpu_temp
+        end
     end
 end
